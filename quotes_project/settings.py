1d0a1789a2c3b007a968ed1b7e52b5a96f04ce2e
Leela3576 <akash98764625@gmail.com>
2025-11-09 20:16:13 +0530

    Prepare quotes_project for Render deployment

--- quotes_project/settings.py
+++ quotes_project/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quote_project.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
